746e9a2ddf8b922d2616d40d1ffa4d75942a2c76
Jeremy Larkin <jeremy.larkin@segment.com>
2020-05-15 12:42:00 -0700

    Create test.js

---
+++ test.js
@@ -0,0 +1,22 @@
+// Learn more about source functions API at
+// https://segment.com/docs/connections/sources/source-functions
+
+/**
+ * Handle incoming HTTP request
+ *
+ * @param  {FunctionRequest} request
+ * @param  {FunctionSettings} settings
+ */
+async function onRequest(request, settings) {
+  const body = request.json()
+  console.log(body)
+
+  // See https://segment.com/docs/connections/spec/track/
+  Segment.track({
+    event: 'Test Event',
+    userId: 'user_id',
+    properties: {
+      testProperty: 'testValue'
+    }
+  })
+}
